18c21a720350fde5d70a00499a3e216f00b927de
GuYith <48597370+GuYith@users.noreply.github.com>
2022-05-09 20:50:23 +0800

    delete invalid file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 import BaiduMap from 'vue-baidu-map';
-import HelloWorld from "@/components/HelloWorld";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from "axios";
